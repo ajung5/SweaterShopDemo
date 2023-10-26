54af53b90b644f004e1ef8be9471e6d0ad0c988d
Agung Nawawi <agung@fairtech.com.sg>
2023-10-26 15:39:51 +0700

    add product data

--- SweaterShopDemo/Models/Product.swift
+++ SweaterShopDemo/Models/Product.swift
@@ -11,6 +11,17 @@ import Foundation
 struct Product: Identifiable {
     var id = UUID()
     var name: String
-    var image: String
+    var imageName: String
     var price: Int
 }
+
+var productList: [Product] = [
+    Product(name: "Oversized Sweater", imageName: "sweater1", price: 150000),
+    Product(name: "Cable Knit Sweater", imageName: "sweater2", price: 75000),
+    Product(name: "Crewneck Sweater", imageName: "sweater3", price: 125000),
+    Product(name: "V-Neck Sweater", imageName: "sweater4", price: 130000),
+    Product(name: "Turtleneck Sweater", imageName: "sweater5", price: 89000),
+    Product(name: "Hoodie Sweater", imageName: "sweater6", price: 99000),
+    Product(name: "Cardigan Sweater", imageName: "sweater7", price: 73500),
+    Product(name: "Cashmere Sweater", imageName: "sweater8", price: 220000)
+]
